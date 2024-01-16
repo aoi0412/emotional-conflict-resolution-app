1e3cd5a394d7a690835b079696bb6bca29758ce0
aoi0412 <aoiaoi0412.1203@gmail.com>
2024-01-16 16:56:41 +0900

    add config of firebase

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { initializeFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || "",
+};
+const firebase = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+initializeFirestore(firebase, {
+  ignoreUndefinedProperties: true,
+});
+
+export const firebaseApp = firebase;
