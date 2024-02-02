35ea753bfbb26c7c84100c0c7240f7a9cd5fd346
aoi0412 <aoiaoi0412.1203@gmail.com>
2024-02-02 13:59:07 +0900

    z

--- app/testEmpath/page.tsx
+++ app/testEmpath/page.tsx
@@ -6,12 +6,37 @@ import { useEffect, useRef } from "react";
 const page = () => {
   const input = useRef<HTMLInputElement>(null);
   useEffect(() => {
-    input.current?.addEventListener("change", (e: any) => {
-      const file = e.target.files[0];
-      console.log(file);
+    const getEmotion = async () => {
+      input.current?.addEventListener("change", async (e: any) => {
+        const file = e.target.files[0];
+        console.log(file);
 
-      uploadFileToChunkEndpoint(file);
-    });
+        try {
+          const formData = new FormData();
+          formData.append("file", file);
+
+          const response = await fetch("/testEmpath/api", {
+            method: "POST",
+            body: formData,
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log("File uploaded successfully!");
+            console.log("Response from server:");
+            console.log(data.talkUnits);
+          } else {
+            console.error(
+              `Failed to upload the file. StatusCode: ${response.status}`
+            );
+            console.error(await response.text());
+          }
+        } catch (error: any) {
+          console.error("Error uploading file:", error.message);
+        }
+      });
+    };
+    getEmotion();
   }, []);
   return (
     <div>
